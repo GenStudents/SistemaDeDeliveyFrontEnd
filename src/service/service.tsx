6c0db27511ccf7f402ec4f3c4d80190677c49c06
Jessica Gizela Dev <devjessgizela@gmail.com>
2026-03-05 18:58:25 -0300

    Corrige import de Categoria e atualiza FormProduto com categorias e disponibilidade

--- src/service/service.tsx
+++ src/service/service.tsx
@@ -1,4 +1,5 @@
 import axios from "axios";
+import { toast } from "react-toastify";
 
 const api = axios.create({
   baseURL: import.meta.env.VITE_API_URL
@@ -6,34 +7,68 @@ const api = axios.create({
 
 // eslint-disable-next-line @typescript-eslint/no-unsafe-function-type
 export const cadastroUsuario = async (url: string, dados: object, setDados: Function) => {
-  const resposta = await api.post(url, dados);
-  setDados(resposta.data);
+  try {
+    const resposta = await api.post(url, dados);
+    setDados(resposta.data);
+  } catch (erro) {
+    console.error("Erro ao cadastrar usuário:", erro);
+    toast.error("Erro ao cadastrar usuário");
+  }
 }
 
 // eslint-disable-next-line @typescript-eslint/no-unsafe-function-type
 export const login = async(url: string, dados: object, setDados:Function) => {
-  const resposta = await api.post(url,dados);
-  setDados(resposta.data)
+  try {
+    const resposta = await api.post(url,dados);
+    setDados(resposta.data)
+  } catch (erro) {
+    console.error("Erro ao fazer login:", erro);
+    toast.error("Erro ao fazer login");
+  }
 }
 
 // eslint-disable-next-line @typescript-eslint/no-unsafe-function-type
 export const buscar = async (url: string, setDados: Function, header: object) => {
+  try {
     const resposta = await api.get(url, header)
     setDados(resposta.data)
+  } catch (erro) {
+    console.error("Erro ao buscar dados:", erro);
+    toast.error("Erro ao carregar dados");
+    setDados([]);
+  }
 }
 
 // eslint-disable-next-line @typescript-eslint/no-unsafe-function-type
 export const cadastrar = async (url: string, dados: object, setDados: Function, header: object) => {
+  try {
     const resposta = await api.post(url, dados, header)
     setDados(resposta.data)
+    toast.success("Cadastro realizado com sucesso!");
+  } catch (erro) {
+    console.error("Erro ao cadastrar:", erro);
+    toast.error("Erro ao cadastrar");
+  }
 }
 
 // eslint-disable-next-line @typescript-eslint/no-unsafe-function-type
 export const atualizar = async (url: string, dados: object, setDados: Function, header: object) => {
+  try {
     const resposta = await api.put(url, dados, header)
     setDados(resposta.data)
+    toast.success("Atualização realizada com sucesso!");
+  } catch (erro) {
+    console.error("Erro ao atualizar:", erro);
+    toast.error("Erro ao atualizar");
+  }
 }
 
 export const deletar = async (url: string, header: object) => {
+  try {
     await api.delete(url, header)
+    toast.success("Deletado com sucesso!");
+  } catch (erro) {
+    console.error("Erro ao deletar:", erro);
+    toast.error("Erro ao deletar");
+  }
 }
